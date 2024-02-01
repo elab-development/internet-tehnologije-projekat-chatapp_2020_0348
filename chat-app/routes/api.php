a607a0a50e87048ecfdbb71c27e27ddfed3ef6cf
MilosFilipovic <filipovicmilos001@gmail.com>
2024-02-01 23:02:10 +0100

    rute i kontroler-Messages

--- chat-app/routes/api.php
+++ chat-app/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\UserAuth\AuthController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,6 +28,9 @@ Route::get('/chats', [ChatController::class, 'index']);
 Route::get('/chats/paginate', [ChatController::class, 'indexPaginate']);
 Route::get('/chats/{id}', [ChatController::class, 'show']);
 
+Route::get('/messages', [MessageController::class, 'index']);
+Route::get('/messages/{id}', [MessageController::class, 'show']);
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -34,9 +38,14 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function (Request $request) {
         return auth()->user();
     });
-    
+
     Route::resource('/chats', ChatController::class)
     ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/messages', MessageController::class)
+    ->only(['store', 'update', 'destroy']);
+
+
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
