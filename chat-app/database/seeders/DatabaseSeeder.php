388b9d20851ce1da06a4fb1405a698aea4c07410
Boslodan <sjankovic01@gmail.com>
2024-02-01 22:12:22 +0100

    sider

--- chat-app/database/seeders/DatabaseSeeder.php
+++ chat-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Chat;
+use App\Models\Message;
+use App\Models\User;
+use App\Models\UserChat;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,108 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Chat::truncate();
+        UserChat::truncate();
+        Message::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+        $user3 = User::factory()->create();
+
+        $chat1 = Chat::factory()->create();
+        $chat2 = Chat::factory()->create();
+        $chat3 = Chat::factory()->create();
+        $chat4 = Chat::factory()->create();
+
+        UserChat::factory()->create([
+            'user_id' => $user1->id,
+            'chat_id' => $chat1->id
+        ]);
+        UserChat::factory()->create([
+            'user_id' => $user2->id,
+            'chat_id' => $chat1->id
+        ]);
+        UserChat::factory()->create([
+            'user_id' => $user1->id,
+            'chat_id' => $chat2->id
+        ]);
+        UserChat::factory()->create([
+            'user_id' => $user3->id,
+            'chat_id' => $chat2->id
+        ]);
+        UserChat::factory()->create([
+            'user_id' => $user3->id,
+            'chat_id' => $chat3->id
+        ]);
+        UserChat::factory()->create([
+            'user_id' => $user2->id,
+            'chat_id' => $chat3->id
+        ]);
+        UserChat::factory()->create([
+            'user_id' => $user1->id,
+            'chat_id' => $chat4->id
+        ]);
+        UserChat::factory()->create([
+            'user_id' => $user2->id,
+            'chat_id' => $chat4->id
+        ]);
+        UserChat::factory()->create([
+            'user_id' => $user3->id,
+            'chat_id' => $chat4->id
+        ]);
+
+        Message::factory()->create([
+            'user_id' => $user1->id,
+            'chat_id' => $chat1->id,
+        ]);
+        Message::factory()->create([
+            'user_id' => $user2->id,
+            'chat_id' => $chat1->id,
+        ]);
+        Message::factory()->create([
+            'user_id' => $user1->id,
+            'chat_id' => $chat1->id,
+        ]);
+
+        Message::factory()->create([
+            'user_id' => $user1->id,
+            'chat_id' => $chat2->id,
+        ]);
+        Message::factory()->create([
+            'user_id' => $user3->id,
+            'chat_id' => $chat2->id,
+        ]);
+        Message::factory()->create([
+            'user_id' => $user1->id,
+            'chat_id' => $chat2->id,
+        ]);
+
+        Message::factory()->create([
+            'user_id' => $user2->id,
+            'chat_id' => $chat3->id,
+        ]);
+        Message::factory()->create([
+            'user_id' => $user2->id,
+            'chat_id' => $chat3->id,
+        ]);
+        Message::factory()->create([
+            'user_id' => $user3->id,
+            'chat_id' => $chat3->id,
+        ]);
+
+
+        Message::factory()->create([
+            'user_id' => $user1->id,
+            'chat_id' => $chat4->id,
+        ]);
+        Message::factory()->create([
+            'user_id' => $user2->id,
+            'chat_id' => $chat4->id,
+        ]);
+        Message::factory()->create([
+            'user_id' => $user3->id,
+            'chat_id' => $chat4->id,
+        ]);
     }
 }
