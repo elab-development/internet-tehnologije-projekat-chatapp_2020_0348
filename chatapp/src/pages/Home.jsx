6ce9c078c9b34d094a5a518fb3b85fa6bca694b8
MilosFilipovic <filipovicmilos001@gmail.com>
2024-08-25 22:34:04 +0200

    spajanje login dela

--- chatapp/src/pages/Home.jsx
+++ chatapp/src/pages/Home.jsx
@@ -3,23 +3,48 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
 import TextInput from '../components/TextInput';
 import './Home.css'; 
+import axios from 'axios';
 
-const Home = ({onLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    if (username.trim() === '' || password.trim() === '') {
-      setError('Username and password cannot be empty.');
-      return;
-    }
-    setError('');
-    onLogin(true); //Postavljanje isloggedin na true nakon prijave
-    navigate('/chat');
-  };
-
+  const Home = ({ onLogin }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+  
+    const handleLogin = async () => {
+      if (username.trim() === '' || password.trim() === '') {
+        setError('Username and password cannot be empty.');
+        return;
+      }
+    
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/login', {
+          email: username,
+          password: password,
+        });
+    
+        if (response.data.access_token) {
+          // Ako je prijava uspešna
+          onLogin(true); // Postavljanje isLoggedIn na true nakon prijave
+          navigate('/chat');
+        } else {
+          // Ako je prijava neuspešna (npr. pogrešni kredencijali)
+          setError('Invalid username or password.');
+        }
+      } catch (error) {
+        // Ako se desi greška tokom zahteva ka serveru
+        if (error.response && error.response.status === 401) {
+          // Ako server vrati 401 Unauthorized, postavite specifičnu poruku o grešci
+          setError('Invalid username or password.');
+        } else {
+          // Za sve ostale greške
+          setError('An error occurred during login. Please try again.');
+          console.error('Login error:', error);
+        }
+      }
+    };
+    
   return (
     <div className="home-page">
       <div className="login-container">
@@ -27,7 +52,7 @@ const Home = ({onLogin }) => {
           <h2>Sign In</h2>
           <TextInput
             id="username"
-            label="Username"
+            label="Email"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
